795b33231093255545c1d8f67d452c37624e7b50
yu-hal <haley.yu@stonybrook.edu>
2023-05-12 23:38:13 +0000

    hope this works

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def home():
-    return 'this is a API service for MN ICD code details'
+    return 'this is a API service for drug details'
 
 @app.route('/preview', methods=["GET"])
 def preview():
@@ -15,21 +15,28 @@ def preview():
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/icd/<value>', methods=['GET'])
-def icdcode(value):
+@app.route('/drugs/<value>/<value2>', methods=['GET'])
+def icdcode(value, value2):
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
+    print('value: ', value2)    
+    filtered = pd.DataFrame()
+    if value == 'dose':
+        filtered = df[df['DOSAGE_FORM'] == value2]
+    elif value == 'name':
+        filtered = df[df['NPROPNAME'] == value2]
+    elif value == 'year':
+        filtered = df[df['LAUNCH_YEAR'] == int(value2)]
     if len(filtered) <= 0:
-        return 'There is nothing here'
+        return 'This does not exist D:'
     else: 
         return filtered.to_json(orient="records")
 
-@app.route('/icd/<value>/sex/<value2>')
+@app.route('/drugs/<value>/dose/<value2>')
 def icdcode2(value, value2):
-    filtered = df[df['principal_diagnosis_code'] == value]
-    filtered2 = filtered[filtered['sex'] == value2]
+    filtered = df[df['NPROPNAME'] == value]
+    filtered2 = filtered[filtered['DOSAGE_FORM'] == value2]
     if len(filtered2) <= 0:
-        return 'There is nothing here'
+        return 'This does not exist D:'
     else: 
         return filtered2.to_json(orient="records")    
     
